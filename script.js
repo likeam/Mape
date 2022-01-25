3d98fc995a0e808061ba5ea2895540d2919025c2
likeam <92205472+likeam@users.noreply.github.com>
2022-01-26 00:10:11 +0500

    ok

--- script.js
+++ script.js
@@ -14,6 +14,55 @@ const inputElevation = document.querySelector('.form__input--elevation');
 
 
 
+class Workout {
+    date = new Date();
+    id = (Date.now() + '').slice(-10);
+
+    constructor(coords, distance, duration){
+
+
+        this.coords = coords;
+        this.distance = distance;
+        this.duration = duration;
+    }
+}
+
+class Running extends Workout {
+    constructor(coords, distance, duration, cadence){
+        super(coords, distance, duration);
+        this.cadence = cadence;
+        this.calcPace();
+    }
+
+    calcPace(){
+
+        this.pace = this.duration / this.distance;
+        return this.pace;
+    }
+}
+
+class Cycling extends Workout {
+    constructor(coords, distance, duration, elevationGain){
+        super(coords, distance, duration);
+   this.elevationGain = elevationGain;
+   this.calcSpeed();
+   }
+   calcSpeed(){
+       this.speed = this.distance / (this.duration /60);
+       return this.speed;
+   }
+
+}
+
+
+const run1 = new Running([39, -12], 5.2, 24, 178);
+const cycling1 = new Cycling([39, -12], 5.2, 24, 178);
+console.log(run1, cycling1);
+
+
+///////////////////////////////////////////////////
+//    APPLICATION ARTITECTURE
+
 class App {
 
     #map;
